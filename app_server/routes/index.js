a686d51e530730e29ab9dd9dfc4a9ce9409fb73f
Ewgen <parshin.ewg@gmail.com>
2019-08-06 05:22:23 +0300

    Deployment WebServ v0.0.4

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var ctrlMain = require('../controllers/main');
 
-/* GET home page. */
-var homepageController = function(req, res, next) {
-  res.render('index', { title: 'Express' });
-}
-router.get('/', homepageController);
+
+
+router.get('/', ctrlMain.index);
 
 module.exports = router;
